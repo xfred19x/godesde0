de7bbca4b47c34fb92271265819c761b146d763f
Freddy Gonzales <fgonzales@soaint.com>
2023-04-02 02:35:08 -0500

    commit Ejercicio01

---
+++ 6.Ejercicio01/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/xfred19x/godesde0/6.Ejercicio01/ejercicios"
+)
+
+func main() {
+
+	fmt.Println("-------------")
+	numero, texto := ejercicios.ConvNumerico("500")
+	fmt.Println(numero)
+	fmt.Println(texto)
+
+}
